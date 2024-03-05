98eb9a4353295eff1585898813e3a82bc17faa42
Mariela-t <tinoco.mariela01@gmail.com>
2024-03-04 22:11:42 -0800

    answers for lesson 1

--- 01-node-tutorial/answers/prompter.js
+++ 01-node-tutorial/answers/prompter.js
@@ -22,15 +22,16 @@ const getBody = (req, callback) => {
 
 // here, you could declare one or more variables to store what comes back from the form.
 let item = "Enter something below.";
+let color = "What is your favorite color?";
 
 // here, you can change the form below to modify the input fields and what is displayed.
 // This is just ordinary html with string interpolation.
 const form = () => {
   return `
   <body>
-  <p>${item}</p>
+  <p>Favorite color:${color}</p>
   <form method="POST">
-  <input name="item"></input>
+  <input name="color" placeholder="Enter favorite color here"></input>
   <button type="submit">Submit</button>
   </form>
   </body>
@@ -44,10 +45,10 @@ const server = http.createServer((req, res) => {
     getBody(req, (body) => {
       console.log("The body of the post is ", body);
       // here, you can add your own logic
-      if (body["item"]) {
-        item = body["item"];
+      if (body["color"]) {
+        color = body["color"];
       } else {
-        item = "Nothing was entered.";
+        color = "Nothing was entered.";
       }
       // Your code changes would end here
       res.writeHead(303, {
