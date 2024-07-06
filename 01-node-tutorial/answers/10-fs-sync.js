285e6904b6fc6386a8b4547244c73ace850d8c93
Mariela-t <tinoco.mariela01@gmail.com>
2024-07-05 22:14:30 -0700

    Revised code week1

--- 01-node-tutorial/answers/10-fs-sync.js
+++ 01-node-tutorial/answers/10-fs-sync.js
@@ -1,11 +1,30 @@
+// const { readFileSync, writeFileSync } = require("fs");
+// console.log("start");
+// const first = readFileSync("./content/first.txt", "utf8");
+// const second = readFileSync("./content/second.txt", "utf8");
+// const third = writeFileSync("./content/third.txt", "utf8");
+
+// writeFileSync(
+//   "./temporary/fileA.txt",
+//   `Here is the result : ${first}, ${second}, ${third}`,
+//   { flag: "a" }
+// );
+
+// console.log("done with this task");
+// console.log("starting the next one");
+// console.log("starting the last one");
+
+
+
 const { readFileSync, writeFileSync } = require("fs");
 console.log("start");
-const first = readFileSync("./content/first.txt", "utf8");
-const second = readFileSync("./content/second.txt", "utf8");
+ writeFileSync("./temporary/fileA.txt", "First\n", { flag: "a" });
+ writeFileSync("./temporary/fileA.txt", "Second\n", { flag: "a" });
+ writeFileSync("./temporary/fileA.txt", "Third\n", { flag: "a" });
 
-writeFileSync(
-    './temporary/fileA.txt', 
-    `Here is the result : ${first}, ${second}`, { flag: 'a' } )
+const fileContent= readFileSync(
+  "./temporary/fileA.txt",'utf8'
+);
 
-console.log("done with this task");
-console.log("starting the next one");
+console.log(fileContent);
+
